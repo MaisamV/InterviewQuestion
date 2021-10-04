f9617a6c21bd91295305189d85baab8b6225f3d4
MaisamV <maisam.vahidsafa@gmail.com>
2021-10-04 10:12:52 +0330

    Fixed description field in NewsImageItem.kt

--- News/src/main/kotlin/com/test/news/NewsImageItem.kt
+++ News/src/main/kotlin/com/test/news/NewsImageItem.kt
@@ -1,3 +1,3 @@
 package com.test.news
 
-class NewsImageItem(id: Int, description: String, val url: String) : NewsItem(id, NewsType.IMAGE)
+class NewsImageItem(id: Int, val description: String, val url: String) : NewsItem(id, NewsType.IMAGE)
